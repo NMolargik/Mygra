ebd828ad0a2c7de2db6a345540296b3ef0249918
Nick Molargik <nick@molargiksoftware.com>
2025-07-13 16:10:10 -0400

    Initial Commit

---
+++ MygraTests/MygraTests.swift
@@ -0,0 +1,16 @@
+//
+//  MygraTests.swift
+//  MygraTests
+//
+//  Created by Nick Molargik on 7/13/25.
+//
+
+import Testing
+
+struct MygraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
